f079ecdeb88a3aff02a22936e89a28aa499e89e7
Dustin McCallum <118634961+dhmac2@users.noreply.github.com>
2024-08-09 15:54:13 -0500

    creation on manage and cleanup of blogmaker_lite

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,17 +1,10 @@
 from pathlib import Path
-import os
 
-import django
-#from django.conf import settings
-from django.http import HttpResponse
+
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management import execute_from_command_line
 from django.shortcuts import render
 
-# Load settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
 
 def index(request):
     return render(request, "index.html")
@@ -22,5 +15,3 @@ urlpatterns = [
 
 application = WSGIHandler()
 
-if __name__ == "__main__":
-    execute_from_command_line()
